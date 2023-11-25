857d4b6f68fe7c62d0a8e045c1df89633c454067
Nguyendz102 <thenguyen9603@gmail.com>
2023-11-25 10:29:50 +0700

    sendEmaiLienHe

--- src/API.js
+++ src/API.js
@@ -1,2 +1,6 @@
 export const URL = "http://localhost:8080"
 export const GET_SIZE = URL +"/api-admin/getSize"
+export const GET_ALL_CONTACT = URL +"/api-admin/contact"
+export const PUT_PHANHOI_LIENHE = URL +"/api-admin/put-phanhoi-contact"
+
+
